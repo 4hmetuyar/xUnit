4ec787cc274feda6d93e0078921da13412fc5958
Ahmet Uyar <4hmetuyar@gmail.com>
2018-08-01 14:10:36 +0300

    Number karşılaştırma işlemiler için örnek eklendi.

---
+++ xUnit.Web/xUnit.Web.Test/NumericAssertsShould.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xUnit.Web.Test
+{
+    public class NumericAssertsShould
+    {
+        [Fact]
+        public void NumberCompareMetod()
+        {
+            int value = 100;
+
+            Assert.Equal(100, value);
+        }
+    }
+}
