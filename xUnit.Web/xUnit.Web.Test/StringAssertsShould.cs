5d6a99e7af7852406dbfe5ee780d75f7229bc0bf
Ahmet Uyar <4hmetuyar@gmail.com>
2018-08-01 14:08:21 +0300

    String Asserts ile iki tane string ifade karşılaştırmak için geliştirme yapıldı.

---
+++ xUnit.Web/xUnit.Web.Test/StringAssertsShould.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace xUnit.Web.Test
+{
+    public class StringAssertsShould
+    {
+
+        /// <summary>
+        /// İki tane string ifadeyi karşılaştırmak için kullanılır.
+        /// </summary>
+        [Fact]
+        public void CompareFullName()
+        {
+            var fullName = "Ahmet Uyar";
+            Assert.Equal("Ahmet Uyar", fullName);
+        }
+    }
+}
