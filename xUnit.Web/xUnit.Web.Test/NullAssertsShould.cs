c3d84532ff0094fd3a6c397c1ae295f485cfbfdb
Ahmet Uyar <4hmetuyar@gmail.com>
2018-08-01 14:14:25 +0300

    Null kontrolü için örnek eklendi.

---
+++ xUnit.Web/xUnit.Web.Test/NullAssertsShould.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace xUnit.Web.Test
+{
+    public class NullAssertsShould
+    {
+        [Fact]
+        public void NotHaveNickNameByDefault()
+        {
+            string user = null;
+            Assert.Null(user);
+        }
+    }
+}
